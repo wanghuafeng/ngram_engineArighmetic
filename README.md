c6beb8aebc1f125acb6d97f8ae498a8120f3db44
wanghuafeng <wachfx@163.com>
2014-10-16 10:48:02 +0800

    add some comment

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ngram_engineArighmetic
 ======================
-github test 
+1¡¢N_gram_engin_arithmetic.py
+	contains all the step of training languageModule
+2¡¢word_request_ali.py
+	(1)convert pinyin to role_num
+	(2)request zhangyu ali cloud word
+	(3)request sougou cloud word 
--- word_request_ali.py
+++ word_request_ali.py
@@ -1,9 +1,11 @@
 #coding:utf-8
 import simplejson
+import os
+import codecs
 import time
 import urllib2
 from add_pinyin_to_single_word import AddPinyin
-import sogou_cloud_words
+from sogou_cloud_words import get_cloud_words
 
 coding_map = {'a': '2', 'c': '2', 'b': '2', 'e': '3', 'd': '3', 'g': '4', 'f': '3', 'i': '4', 'h': '4', 'k': '5', 'j': '5', 'm': '6', 'l': '5', 'o': '6', 'n': '6', 'q': '7', 'p': '7', 's': '7', 'r': '7', 'u': '8', 't': '8', 'w': '9', 'v': '8', 'y': '9', 'x': '9', 'z': '9'}
 
@@ -30,9 +32,10 @@ def post_to_local(role_num):
 
 if __name__ == "__main__":
     start_time = time.time()
-    sentence = u'我吓坏了'
+    sentence = u'学挖掘机到底哪家强中国山东找蓝翔'
     role_num = get_pinyin(sentence)
-    # role_num = '366428332472436832338247484'
+    # role_num = '945464983248432694224336335433826'
     # role_num = 'jintianyourenqudapingpangqiuma'
     post_to_ali(role_num)
     # post_to_local(role_num)
+    print get_cloud_words(role_num)[0]
